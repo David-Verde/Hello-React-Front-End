18b0e0f57844cf7a0e8a757b28bd59cc2add0b3c
Unyielding1 <david.verde.alvarez@gmail.com>
2022-11-01 18:42:27 -0500

    main Setup and Milestone

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+
+import './App.css';
+import Greeting from './components/Greeting';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
